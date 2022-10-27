8ec54399739c503912090886337ee2874f84492f
Maria Litwa <contact@marialitwa.com>
2022-10-27 11:56:11 +0200

    Initial commit

---
+++ pages/dessert/index.js
@@ -0,0 +1,7 @@
+export default function Dessert() {
+  return;
+  <>
+    <h1>Dessert Page</h1>
+    <p>Dessert Menu</p>;
+  </>;
+}
---
+++ pages/veggie/index.js
@@ -0,0 +1,7 @@
+export default function Veggie() {
+  return;
+  <>
+    <h1>Veggie Page</h1>
+    <p>Veggie Menu</p>;
+  </>;
+}
