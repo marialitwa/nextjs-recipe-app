0fb8b99cc7235bd76aacbb9e82dd8d0426d5a26f
Maria Litwa <contact@marialitwa.com>
2022-10-27 16:03:15 +0200

    Import image file

--- pages/veggie/index.js
+++ pages/veggie/index.js
@@ -1,7 +1,32 @@
+import Image from "next/image";
+
 export default function Veggie() {
-  return;
-  <>
-    <h1>Veggie Page</h1>
-    <p>Veggie Menu</p>;
-  </>;
+  return (
+    <>
+      <h1>Veggie Page</h1>
+      <p>Veggie Menu</p>;
+      <Background>
+        <Image
+          src="/images/pizza-photo-by-laure-noverraz.jpg"
+          alt="Leckere
+      Pizza"
+          width={2400}
+          height={1800}
+        />
+      </Background>
+    </>
+  );
 }
+
+const Background = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  overflow-y: hidden;
+  top: 0;
+  left: 0;
+  z-index: -10;
+  @media (max-width: 600px) {
+    display: none;
+  }
+`;
